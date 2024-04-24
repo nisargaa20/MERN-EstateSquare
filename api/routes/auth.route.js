f771c249c78b5c2de42c6927e0214358ac55ec94
nisargaa20 <125184228+nisargaa20@users.noreply.github.com>
2024-04-24 06:54:27 +0530

    created signup api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,13 +1,9 @@
 import express from 'express';
-import {test} from '../controllers/user.controller.js';
 import { signup } from '../controllers/auth.controller.js';
-
-
 
 
 const router = express.Router();
 
-router.get('/signup', signup);
-
+router.post("/signup",signup);
 
 export default router;
