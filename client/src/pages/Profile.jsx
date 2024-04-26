5cec003ff051ff33d0e2bff6ee4740e77988a15b
nisargaa20 <125184228+nisargaa20@users.noreply.github.com>
2024-04-26 21:50:46 +0530

    completed update listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -282,7 +282,9 @@ const handleListingDelete = async (listingId) => {
                 >
                   Delete
                 </button>
-                <button className='text-green-700 uppercase'>Edit</button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className='text-green-700 uppercase'>Edit</button>
+                </Link>
               </div>
             </div>
           ))}
